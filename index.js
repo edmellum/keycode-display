9f0fedcbaa56965314a994b63caefa42c136953c
David Ed Mellum <david@edmellum.com>
2013-04-24 19:43:59 +0200

    Added initial encouragement

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var template = require('./template');
 module.exports = function() {
   var el = domify(template)[0];
 
+  el.innerHTML = 'Press a key!';
+
   events.bind(document.body, 'keydown', function(event) {
     el.innerHTML = event.keyCode;
   });
--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ var happen = require('tmcw-happen');
 var keyDisplay = require('keycode-display');
 
 describe('keycode-display', function() {
+  it('should display text before a key has been pressed', function() {
+    var el = keyDisplay();
+    assert.equal(el.innerHTML, 'Press a key!');
+  });
+
   it('should display the keyCode of the pressed key', function() {
     var el = keyDisplay();
 
